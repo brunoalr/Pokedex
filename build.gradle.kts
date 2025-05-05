7e0c6fa769a895587518d869b458f6dffa63f201
Bruno Alexandre Rosa <1791393+brunoalr@users.noreply.github.com>
2025-05-05 19:33:27 -0300

    Bump AGP

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
     kotlin("multiplatform") version "1.9.20" apply false
     kotlin("plugin.serialization") version "1.9.20" apply false
     id("app.cash.sqldelight") version "2.0.0" apply false
-    id("com.android.application") version "8.1.3" apply false
-    id("com.android.library") version "8.1.3" apply false
+    id("com.android.application") version "8.9.2" apply false
+    id("com.android.library") version "8.9.2" apply false
     id("org.jetbrains.compose") version "1.5.10" apply false
 }
 

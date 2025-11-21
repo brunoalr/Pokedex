bbdbc7f0375c25045912a418c206dc54aafa3d29
Bruno Alexandre Rosa <1791393+brunoalr@users.noreply.github.com>
2025-11-21 12:45:57 -0300

    bump versions

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -6,11 +6,11 @@ plugins {
 
 android {
     namespace = "com.mocoding.pokedex.android"
-    compileSdk = 35
+    compileSdk = 36
     defaultConfig {
         applicationId = "com.mocoding.pokedex.android"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0.0"
     }
